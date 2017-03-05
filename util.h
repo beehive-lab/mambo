fd8088cc52ef98bd48f61608e3abd5fe89f1fbfa
Cosmin Gorgovan <cosmin@linux-geek.org>
2017-03-05 18:23:25 +0000

    Atomic operations: add definitions for more data types

--- util.h
+++ util.h
@@ -23,11 +23,19 @@
 extern void dbm_client_entry(uintptr_t addr, uintptr_t *stack_top);
 extern uint32_t atomic_increment_u32(uint32_t *loc, uint32_t inc);
 extern uint64_t atomic_increment_u64(uint64_t *loc, uint64_t inc);
+inline int32_t atomic_increment_i32(int32_t *loc, int32_t inc) {
+  return (int32_t)atomic_increment_u32((uint32_t *)loc, (uint32_t)inc);
+}
+inline int64_t atomic_increment_i64(int64_t *loc, int64_t inc) {
+  return (int64_t)atomic_increment_u64((uint64_t *)loc, (uint64_t)inc);
+}
 #ifdef __arm__
   #define atomic_increment_uptr(loc, inc) atomic_increment_u32(loc, inc);
 #elif __aarch64__
   #define atomic_increment_uptr(loc, inc) atomic_increment_u64(loc, inc);
 #endif
+#define atomic_increment_int(loc, inc) atomic_increment_i32(loc, inc);
+#define atomic_increment_uint(loc, inc) atomic_increment_u32(loc, inc);
 
 // syscall() without errno handling
 extern uintptr_t raw_syscall(long number, ...);
