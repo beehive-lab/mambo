084246f25202253c8f7559e7dcc300c38210faf6
umarcor <unai.martinezcorral@ehu.eus>
2020-05-07 12:32:08 +0200

    implement function replacement using the high-level API

--- plugins/function_replacement.c
+++ plugins/function_replacement.c
@@ -2,6 +2,7 @@
   This file is part of MAMBO, a low-overhead dynamic binary modification tool:
       https://github.com/beehive-lab/mambo
 
+  Copyright 2018-2020 Cosmin Gorgovan <cosmin at linux-geek dot org>
   Copyright 2018-2020 Guillermo Callaghan <guillermocallaghan at hotmail dot com>
   Copyright 2018-2020 Unai Martinez-Corral <unai.martinezcorral at ehu dot eus>
   Copyright 2018-2020 The University of Manchester
@@ -35,101 +36,14 @@ extern void func_name(func_argd);
 
 int func_handler(mambo_context *ctx) {
 
-  uint32_t *wrp = (uint32_t *)ctx->code.write_p;
-
-#ifdef __arm__
-
-  int inst_set = mambo_get_inst_type(ctx);
-
-  if (inst_set == ARM_INST) {
-
-    uint32_t *write_p = wrp;
-
-    arm_push_regs((1 << es) | (1 << lr));
-
-    arm_copy_to_reg_32bit(
-    &write_p,
-    es,
-    (uintptr_t)func_name
-  );
-    arm_blx(
-    &write_p,
-    es
-  );
-
-    write_p++;
-
-    arm_pop_regs((1 << es) | (1 << lr));
-    arm_inline_hash_lookup(
-    ctx->thread_data,
-    (uint32_t**)&write_p,
-    ctx->code.fragment_id,
-    lr
-  );
-    wrp = write_p;
-
-  } else if (inst_set == THUMB_INST) {
-
-    uint16_t *write_p = (uint16_t *)wrp;
-
-    thumb_push_regs(&write_p, (1 << es) | (1 << lr));
-
-    copy_to_reg_32bit(
-    &write_p,
-    es,
-    (uintptr_t)func_name
-  );
-    thumb_blx16(
-    &write_p,
-    es
-  );
-
-    write_p++;
-
-    thumb_pop_regs(&write_p, (1 << es) | (1 << lr));
-    thumb_inline_hash_lookup(
-    ctx->thread_data,
-    &write_p,
-    ctx->code.fragment_id,
-    lr
-  );
-    wrp = (uint32_t*)write_p;
-
-  }
-
-#elif __aarch64__
-
-  uint32_t *write_p = wrp;
-
-  a64_push_pair_reg(es, lr)
-
-  a64_copy_to_reg_64bits(
-    &write_p,
-  es,
-  (uintptr_t)func_name
-  );
-  a64_BLR(
-    &write_p,
-  es
-  );
-
-  write_p++;
-
-  a64_pop_pair_reg(es, lr)
-  a64_inline_hash_lookup(
-    ctx->thread_data,
-  ctx->code.fragment_id,
-  &write_p,
-    ctx->code.read_address,
-  lr,
-  false,
-  true
-  );
-  wrp = write_p;
-
-#endif
-
-  ctx->code.write_p = (void *)wrp;
+  /* specifying MAX_FCALL_ARGS so that only the LR is saved on the stack,
+     without the other caller-saved registers
+     safe because the application was compiled already expecting a function entry here
+     */
+  emit_safe_fcall(ctx, func_name, MAX_FCALL_ARGS);
+  // edit: any post-function code goes here instead of the post-function callback
+  emit_indirect_branch_by_spc(ctx, lr);
+  mambo_stop_scan(ctx);
 
   return 0;
 
