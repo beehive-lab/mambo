2b7c676e4793941bff13c2b0f0040967699cdafe
Christopher Peredy <16451370+mastercaution@users.noreply.github.com>
2022-03-15 14:32:16 +0100

    Ignore ELF cache vectors (glibc 2.34+)
    Fix unhandled ELF vector types on Linux kernel 5.12+ with glibc 2.34+.

--- elf/elf_loader.c
+++ elf/elf_loader.c
@@ -398,7 +398,18 @@ void elf_run(uintptr_t entry_address, char *filename, int argc, char **argv, cha
 
       case AT_ENTRY:
         d_aux->a_un.a_val = auxv->at_entry;
-        break;  
+        break;
+
+      case AT_L1I_CACHESIZE:
+      case AT_L1I_CACHEGEOMETRY:
+      case AT_L1D_CACHESIZE:
+      case AT_L1D_CACHEGEOMETRY:
+      case AT_L2_CACHESIZE:
+      case AT_L2_CACHEGEOMETRY:
+      case AT_L3_CACHESIZE:
+      case AT_L3_CACHEGEOMETRY:
+        // Ignore
+        break;
 
       default:
         #ifdef __arm__
