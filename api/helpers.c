6b74c7b8b4601f5578b86062957ba8977f5b1913
Cosmin Gorgovan <cosmin@linux-geek.org>
2023-04-22 17:21:10 +0000

    Minor fixes to eeecafb
    Closes #91

--- api/helpers.c
+++ api/helpers.c
@@ -359,7 +359,6 @@ int __emit_branch_cond(inst_set inst_type, void *write, uintptr_t target, mambo_
   if (cond == AL) {
     if (diff < -134217728 || diff > 134217724) return -1;
     a64_branch_helper(write, target, link);
-    //a64_b_helper(write, target);
   } else {
     if (diff < -1048576 || diff > 1048572) return -1;
     a64_b_cond_helper(write, target, cond);
@@ -376,10 +375,6 @@ int __emit_branch_cond(inst_set inst_type, void *write, uintptr_t target, mambo_
 #endif
   return 0;
 }
-
-
-
-
 
 void emit_fcall(mambo_context *ctx, void *function_ptr) {
   // First try an immediate call, and if that is out of range then generate an indirect call
