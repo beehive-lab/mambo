1d43cfe5de4bfd7f62959c776ff7aad2ed1d0ef0
Ivan Zhechev <ijan1@riseup.net>
2023-06-01 15:31:50 +0100

    A64: Add FEAT_LRCPC instructions
    Armv8.3 Load-Acquire RCpc instructions
    
    Instructions added:
    - LDAPR
    - LDAPRH
    - LDAPRB

--- arch/aarch64/scanner_a64.c
+++ arch/aarch64/scanner_a64.c
@@ -947,6 +947,7 @@ size_t scan_a64(dbm_thread *thread_data, uint32_t *read_address,
       case A64_SIMD_X_INDEXED:
       case A64_SWP:
       case A64_SYS:
+      case A64_LRCPC_1:
         a64_copy();
         break;
 
